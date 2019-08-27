9d45205aa25deb59392329adea84d629c4c5cb79
One-Meter-Sunlight <chenkaisupervip@163.com>
2019-08-27 21:49:49 +0800

    init

--- src/main/java/com/imooc/power/config/PageConfig.java
+++ src/main/java/com/imooc/power/config/PageConfig.java
@@ -1,7 +1,6 @@
 package com.imooc.power.config;
 
 import com.baomidou.mybatisplus.plugins.PaginationInterceptor;
-import com.baomidou.mybatisplus.plugins.PerformanceInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -24,9 +23,9 @@ public class PageConfig {
      * SQL执行效率插件
      * 开发环境使用，用于输出 SQL 语句及起执行时间
      */
-    @Bean
+    /*@Bean
     public PerformanceInterceptor performanceInterceptor() {
         return new PerformanceInterceptor();
-    }
+    }*/
 
 }
